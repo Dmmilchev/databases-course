d92de7d5fa468c5fd170fac5c95fdecc9ae52706
Dmmilchev <deo1508@abv.bg>
2024-03-09 16:50:04 +0200

    added revision/week01

---
+++ revision/week01/task01.sql
@@ -0,0 +1,5 @@
+﻿--Напишете заявка, която извежда имената всички изпълнители на филми с нетна стойност над 10 000 000 долара
+SELECT NAME FROM MOVIEEXEC WHERE NETWORTH > 10000000
+
+--Напишете заявка, която извежда имената на всички актьори, които са мъже или живеят в Malibu
+SELECT NAME FROM MOVIESTAR WHERE GENDER = 'M' OR ADDRESS ='malibu'
