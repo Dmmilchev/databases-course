d70252785e66f53502f43627555a393126e3f2d9
Dmmilchev <deo1508@abv.bg>
2024-03-09 16:36:52 +0200

    added week03/task03

---
+++ week03/task03.sql
@@ -0,0 +1,28 @@
+﻿/*
+
+1. Напишете заявка, която извежда страните, чиито кораби са с най-голям брой оръжия.
+2. Напишете заявка, която извежда класовете, за които поне един от корабите им е потънал в битка.
+3. Напишете заявка, която извежда имената на корабите с 16 инчови оръдия (bore).
+4. Напишете заявка, която извежда имената на битките, в които са участвали кораби от клас ‘Kongo’.
+
+*/
+
+/*FIRST TASK*/
+SELECT COUNTRY
+FROM CLASSES
+WHERE NUMGUNS = (SELECT TOP 1 NUMGUNS FROM CLASSES)
+
+/*SECOND TASK*/
+SELECT CLASS
+FROM SHIPS
+WHERE NAME IN (SELECT SHIP FROM OUTCOMES WHERE RESULT = 'sunk')
+
+/*THIRD TASK*/
+SELECT NAME
+FROM SHIPS
+WHERE CLASS IN (SELECT CLASS FROM CLASSES WHERE BORE = 16)
+
+/*FOURTH TASK*/
+SELECT BATTLE 
+FROM OUTCOMES
+WHERE SHIP IN (SELECT CLASS FROM SHIPS)
