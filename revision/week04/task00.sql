f7de9c1a833e94886c1be2fe352c15979e0ca3f0
Dmmilchev <deo1508@abv.bg>
2024-05-09 10:02:33 +0300

    revision week04

---
+++ revision/week04/task00.sql
@@ -0,0 +1,31 @@
+﻿/*
+Задача 1:
+За всяка държава да се изведат имената на корабите, които никога не са участвали в битка.
+*/
+SELECT c.CLASS, s.NAME
+FROM CLASSES c
+JOIN SHIPS s
+ON c.CLASS = s.CLASS 
+LEFT OUTER JOIN OUTCOMES o ON s.NAME = o.SHIP
+WHERE o.BATTLE IS NULL
+
+/*
+Задача 2: 
+За всеки клас, да се изведат името му, държавата и имената на всички негови кораби,
+пуснати през 1916г. В резултата да участват и класовете без кораби пуснати през 1916г.
+*/
+SELECT c.CLASS, c.COUNTRY, s.NAME
+FROM CLASSES c LEFT OUTER JOIN SHIPS s
+ON c.CLASS = s.CLASS
+WHERE s.LAUNCHED = 1916
+
+/*
+Задача 3: 
+За всеки клас британски кораби - да се извлече името на класа и имената на всички битки, 
+в които са участвали кораби от този клас. Ако за даден клас няма кораби или те не са участвали 
+в битка - да се включат със стойност NULL за името на битката
+*/
+SELECT s.CLASS, o.BATTLE
+FROM SHIPS s RIGHT OUTER JOIN OUTCOMES o
+ON s.NAME = o.SHIP
+WHERE s.CLASS = 'Gt.Britain'
---
+++ revision/week04/task01.sql
@@ -0,0 +1,40 @@
+﻿-- 1.1. Напишете заявка, която за всеки филм, по-дълъг от 120 минути,
+--      извежда заглавие, година, име и адрес на студио.
+SELECT m.TITLE, m.YEAR, m.STUDIONAME, s.ADDRESS
+FROM MOVIE m JOIN STUDIO s
+ON m.STUDIONAME = s.NAME
+WHERE m.LENGTH > 120
+
+-- 1.2. Напишете заявка, която извежда името на студиото и имената на
+--      актьорите, участвали във филми, произведени от това студио,
+--      подредени по име на студио.
+SELECT DISTINCT s.STARNAME, m.STUDIONAME
+FROM STARSIN s JOIN MOVIE m
+ON s.MOVIETITLE = m.TITLE
+ORDER BY m.STUDIONAME ASC
+
+-- 1.3. Напишете заявка, която извежда имената на продуцентите на филмите,
+--      в които е играл Harrison Ford.
+SELECT DISTINCT me.NAME
+FROM MOVIEEXEC me 
+	JOIN MOVIE m ON me.CERT# = m.PRODUCERC#
+	JOIN STARSIN s ON m.TITLE = s.MOVIETITLE
+WHERE s.STARNAME = 'Harrison Ford'
+
+-- 1.4. Напишете заявка, която извежда имената на актрисите, играли във
+--      филми на MGM.
+SELECT DISTINCT ms.NAME
+FROM MOVIESTAR ms 
+	JOIN STARSIN s ON ms.NAME = s.STARNAME
+	JOIN MOVIE m ON s.MOVIETITLE = m.TITLE
+WHERE m.STUDIONAME = 'MGM' AND ms.GENDER = 'F'
+
+
+-- 1.5. Напишете заявка, която извежда името на продуцента и имената на
+--      филмите, продуцирани от продуцента на 'Star Wars'.
+SELECT *
+FROM MOVIE m
+JOIN MOVIEEXEC e ON e.CERT# = m.PRODUCERC#
+WHERE m.PRODUCERC# = (SELECT PRODUCERC# 
+                      FROM MOVIE 
+					  WHERE TITLE = 'Star Wars' AND YEAR = 1977)
